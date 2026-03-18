c99b988cc7631c35c3ee5718eb620ef7ad605b0f
danny-23-dan <danielprasadj238@gmail.com>
2026-03-18 11:40:58 +0530

    Initialize taxi fare prediction app with data loading

---
+++ ml_taxi_app.py
@@ -0,0 +1,24 @@
+import streamtit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error
+
+st.title(" PragyanAI Taxi Fare Prediction App (End-to-End ML)")
+@st.cache data
+def load data():
+  url = "taxis.csv"
+  df = pd.read_csv(url)
+  df = df.convert_dtypes()
+  st.write(df.head())
+  return df
+df = load_data()
+st.subheader("PragyanAI Dataset Preview")
+
+df = df[['distance', 'fare']].dropna()
+df['distance"]= pd.to_numeric(df['distance'], errors='coerce')
+df['fare'] = pd.to_numeric(df['fare'], errors='coerce')
+x = df[['distance']]
+y = df['fare']
